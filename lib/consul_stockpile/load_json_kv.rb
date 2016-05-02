7878d70fcd661f248815f31d2bd82602533a9540
Ryan Schlesinger <ryan@ryanschlesinger.com>
2016-05-02 12:18:58 -0700

    Add args to LoadJsonKV

--- lib/consul_stockpile/load_json_kv.rb
+++ lib/consul_stockpile/load_json_kv.rb
@@ -2,6 +2,12 @@ require 'consul_stockpile/base'
 
 module ConsulStockpile
   class LoadJsonKV < Base
+    attr_accessor :json
+
+    def initialize(json:)
+      self.json = json
+    end
+
     def call
       puts 'load json kv'
     end
