5144c4d5b5d000be6a0400d569467ca106faf761
Ryan Schlesinger <ryan@ryanschlesinger.com>
2016-05-02 11:25:57 -0700

    Handle fog-local permissions

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,9 +5,13 @@ if consul_stockpile help "$1" 2>&1 | grep -q "consul_stockpile $1"; then
   set -- consul_stockpile "$@"
 fi
 
-# Enable this to run as an unprivileged user
 if [ "$1" = 'consul_stockpile' ]; then
+  # Enable this to run as an unprivileged user
   set -- gosu stockpile "$@"
+
+  if [ -n "$FOG_LOCAL" ]; then
+    chown -R stockpile:stockpile /fog
+  fi
 fi
 
 exec "$@"
